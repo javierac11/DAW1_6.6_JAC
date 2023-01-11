91efa9f0406d06cfe16725c974ff843e4003d12f
javierac11 <javieracedocastrillon@gmail.com>
2023-01-11 22:50:09 +0100

    Terminando

--- main.kt
+++ main.kt
@@ -1,15 +1,41 @@
-class Pila (val pila: MutableList<Int> = mutableListOf()) {
 
-    fun Push (input: Int) {
-        pila.add(input)
+class Pila {
+    val pila = mutableListOf<Int>()
+    fun push(elemento:Int) {
+        pila.add(elemento)
+    }
+    fun pop():Int?{
+        if (pila.size > 0){
+        return {pila.removeAt(pila.lastIndex)}}
+        else null
+    }
+    fun vacia():String{
+        if (pila.size == 0){
+            return "Vacia"
+        }
+        else {
+            return "Llena"
+        }
+
+    }
+    fun tope {
+        return pila[pila.lastIndex]
     }
 
+
+    override fun toString():String {
+
+        return pila.toString()
+
+    }
 
 }
 fun main() {
     var p = Pila()
-    p.Push(4)
-    p.Push(8)
-    p.Push(5)
-    println(p.pila.toString())
+    p.push(4)
+    p.push(8)
+    p.push(5)
+    println(p)
+    p.pop()
+    println(p)
 }
